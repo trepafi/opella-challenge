46aa504504e96ba5b789c250fa01de7552dea39b
Lubert Palacios <lubert.palacios@gmail.com>
2026-03-30 13:07:54 +0200

    Update storate account name

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-opella-tfstate"
-    storage_account_name = "stopellatfstate"
+    storage_account_name = "stopellatfstatelubert"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
   }
